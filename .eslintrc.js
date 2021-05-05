f80d521856d7287a1630006fe27f9dd4cbf1821a
Andrew.Hung <flowingcash911@gmail.com>
2021-05-05 10:12:24 +0800

    [設定] 設定eslint, stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,82 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
-      ],
-      env: {
-        jest: true,
-      },
+    root: true,
+    env: {
+        node: true
     },
-  ],
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/airbnb',
+        'plugin:vue/strongly-recommended',
+        'plugin:vue/recommended',
+        'eslint:recommended',
+        'airbnb-base'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    plugins: ['vue'],
+    rules: {
+    /* common */
+        indent: [2, 4, { SwitchCase: 1 }],
+        'no-unused-vars': 1,
+        'comma-dangle': [2, 'never'],
+        'arrow-body-style': 2,
+        'global-require': 0,
+        'max-len': 0,
+        'no-alert': 0,
+        'no-console': 0,
+        'no-new': 0,
+        'object-curly-newline': 0,
+        'no-restricted-globals': 0,
+        'no-underscore-dangle': [0, {
+            allowAfterThis: true
+        }],
+        camelcase: 0,
+
+        /* import */
+        'import/newline-after-import': 2,
+        'import/prefer-default-export': 1,
+        'import/extensions': [2, 'always', {
+            js: 'never',
+            vue: 'never'
+        }],
+        'import/no-dynamic-require': 0,
+        'import/no-extraneous-dependencies': [2, {
+            optionalDependencies: ['test/unit/index.js']
+        }],
+        /* vue plugin */
+        'vue/no-parsing-error': 0,
+        'vue/no-v-html': 0,
+        'vue/attributes-order': 0,
+        'vue/html-indent': [1, 4],
+        'vue/attribute-hyphenation': [2, 'never'],
+        'vue/max-attributes-per-line': [
+            1,
+            {
+                singleline: 2,
+                multiline: {
+                    max: 1,
+                    allowFirstLine: true
+                }
+            }
+        ],
+        'vue/html-self-closing': [2, { html: { void: 'always' } }],
+        'vue/singleline-html-element-content-newline': 0,
+        'vue/no-multiple-template-root': 0,
+        'no-continue': 0,
+        'linebreak-style': 0,
+        'object-shorthand': 0,
+        'no-nested-ternary': 0
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home mx-auto bg-cyan-300 sm:bg-cyan-300 tablet:bg-gg-light">
-    <img class="mx-auto" alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img class="mx-auto"
+             alt="Vue logo"
+             src="../assets/logo.png"
+        />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '@/components/HelloWorld';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+    name: 'Home',
+    components: {
+        HelloWorld
+    }
 };
 </script>
